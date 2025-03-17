35a6416d66c259db2e847e34ec5dc41879f9cc4e
Ederson D. Wilke <zzezinho26@gmail.com>
2025-03-17 18:06:32 -0400

    portifolio

---
+++ JAVASCRIPT.js
@@ -0,0 +1,15 @@
+var l = document.getElementById ('linkedin')
+var g = document.getElementById ('github')
+var c = document.getElementById ('contato')
+var lin = l
+
+function fimteste(){
+    lin = l
+}
+
+function teste(){
+       
+    lin.innerHTML = (`Olá, aqui voce acessa meu LinkedIn e pode saber mais sobre minha trajetoria e qualificações!`)
+
+}
+
